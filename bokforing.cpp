8b5188974423734e51dfac761452687f4a4f2cdc
Anish Sahoo <anishsahoo2005@gmail.com>
2023-07-18 16:09:06 -0500

    new commit

--- bokforing.cpp
+++ bokforing.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    int n,q;
-    cin>>n>>q>>ws;
-    int* array = new int[n];
+int main() {
+    int n, q;
+    cin >> n >> q;
+    map<int, int> m;
 
+    int init = 0;
+    string query;
+
+    for (int i = 0; i < q; i++) {
+        cin >> query;
+        if (query == "SET") {
+            int q1, q2;
+            cin >> q1 >> q2;
+            m[q1] = q2;
+        }
+        else if (query == "PRINT") {
+            int q1;
+            cin >> q1;
+            if (m.find(q1) != m.end()) {
+                cout << m.at(q1) << endl;
+            } 
+            else cout << init << endl;
+        } 
+        else {
+            int q1;
+            cin >> q1;
+            init = q1;
+            m.clear();
+        }
+    }
     return 0;
 }
